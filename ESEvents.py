0c3ddb8a5b3a318c2e181dba1add45218d6ac19b
Vincent Frammery <vincent.frammery@cgos.asso.fr>
2023-12-09 16:42:37 +0100

    Add import es_systems.cfg to switch ES param to ES folder to check marquee images system

--- ESEvents.py
+++ ESEvents.py
@@ -5,6 +5,7 @@ import os
 import json
 import urllib.parse
 import shlex
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 config = configparser.ConfigParser()
@@ -12,6 +13,22 @@ config = configparser.ConfigParser()
 def load_config():
     config.read('events.ini')
     print("Configuration chargée.")
+
+def load_systems_config(xml_relative_path):
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    xml_path = os.path.join(script_dir, xml_relative_path)
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+
+    system_folders = {}
+
+    for system in root.findall('system'):
+        name = system.find('name').text
+        path = system.find('path').text
+        folder_rom_name = os.path.basename(os.path.normpath(path.strip('~\\..')))
+        system_folders[name] = folder_rom_name
+
+    return system_folders
 
 def launch_media_player():
     kill_command = config['Settings']['MPVKillCommand']
@@ -42,12 +59,13 @@ def ensure_mpv_running():
         launch_media_player()
 
 def escape_file_path(path):
-    """Échappe les caractères spéciaux dans un chemin de fichier pour une utilisation en ligne de commande."""
     return shlex.quote(path)
 
-def find_marquee_file(system_name, game_name):
+def find_marquee_file(system_name, game_name, systems_config):
+    folder_rom_name = systems_config.get(system_name, system_name)
+
     marquee_structure = config['Settings']['MarqueeFilePath']
-    marquee_path = marquee_structure.format(system_name=system_name, game_name=game_name)
+    marquee_path = marquee_structure.format(system_name=folder_rom_name, game_name=game_name)
     full_marquee_path = os.path.join(config['Settings']['MarqueeImagePath'], marquee_path)
     print(f"Chemin du marquee du jeu : {full_marquee_path}")
     marquee_file = find_file(full_marquee_path)
@@ -56,7 +74,7 @@ def find_marquee_file(system_name, game_name):
         return marquee_file
 
     marquee_structure = config['Settings']['SystemFilePath']
-    marquee_path = marquee_structure.format(system_name=system_name)
+    marquee_path = marquee_structure.format(system_name=folder_rom_name)
     full_marquee_path = os.path.join(config['Settings']['SystemMarqueePath'], marquee_path)
     print(f"Chemin du marquee du système : {full_marquee_path}")
     marquee_file = find_file(full_marquee_path)
@@ -76,7 +94,7 @@ def find_file(base_path):
     print(f"Aucun fichier trouvé pour : {base_path}")
     return None
 
-def parse_path(params):
+def parse_path(params, systems_config):
     system_detected = False
     game_detected = False
     for param in params.values():
@@ -85,13 +103,12 @@ def parse_path(params):
         formatted_path = os.path.normpath(decoded_param)
         print(f"Chemin formaté : {formatted_path}")
 
-        # Vérifier d'abord si le paramètre est un nom de système
-        if os.path.isdir(os.path.join(config['Settings']['RomsPath'], decoded_param)):
+        folder_rom_name = systems_config.get(decoded_param, '')
+        if os.path.isdir(os.path.join(config['Settings']['RomsPath'], folder_rom_name)):
             print(f"Nom du système détecté : {decoded_param}")
             system_detected = True
             system_name = decoded_param
 
-        # Puis vérifier si c'est un chemin de fichier
         if os.path.isfile(formatted_path):
             game_detected = True
             path_parts = formatted_path.split(os.sep)
@@ -103,7 +120,6 @@ def parse_path(params):
     if system_detected:
         return system_name, ''
 
-    # Si ni un jeu ni un système n'a été détecté, utiliser le premier paramètre comme chaîne
     if not game_detected and not system_detected and params:
         first_param = next(iter(params.values()))
         print(f"Utilisation du premier paramètre comme chaîne : {first_param}")
@@ -112,10 +128,10 @@ def parse_path(params):
     print("Aucun chemin de fichier valide trouvé dans les paramètres.")
     return '', ''
 
-def execute_command(action, params):
+def execute_command(action, params, systems_config):
     if action in config['Commands']:
-        system_name, game_name = parse_path(params)
-        marquee_file = find_marquee_file(system_name, game_name)
+        system_name, game_name = parse_path(params, systems_config)
+        marquee_file = find_marquee_file(system_name, game_name, systems_config)
         escaped_marquee_file = escape_file_path(marquee_file)
         command = config['Commands'][action].format(
             marquee_file=escaped_marquee_file,
@@ -131,9 +147,10 @@ def handle_request():
     ensure_mpv_running()
     action = request.args.get('event', '')
     params = dict(request.args)
-    print(f"Action reçue : {action}, Paramètres : {params}")  # Imprimer les paramètres reçus
+    print(f"Action reçue : {action}, Paramètres : {params}")
     params.pop('event', None)
-    return execute_command(action, params)
+    systems_config = load_systems_config(os.path.join(config['Settings']['RetroBatPath'], 'emulationstation', '.emulationstation', 'es_systems.cfg'))
+    return execute_command(action, params, systems_config)
 
 if __name__ == '__main__':
     load_config()
