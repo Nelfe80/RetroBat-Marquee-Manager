0baca34b92e5680606f70bdb0afb01abdbe5e736
Vincent Frammery <vincent.frammery@cgos.asso.fr>
2025-03-26 23:42:17 +0100

    Update Start.bat

--- ESEventsScrapTopper.py
+++ ESEventsScrapTopper.py
@@ -159,8 +159,8 @@ def scrape_marquee(game_system, game_title, game_name, marquee_path, full_marque
 
     # Préparation des paramètres pour l'appel API
     api_params = {
-        'devid': 'Nelfe',
-        'devpassword': 'y8dI5PyYsyM',
+        'devid': '',
+        'devpassword': '',
         'softname': 'Retrobat-Marquee-Manager-v.3.0',
         'output': 'json',
         'ssid': es_settings['ScreenScraperUser'],
